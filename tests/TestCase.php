a267c0e06218470b14dcb399beace9ab7da00e0c
Chikondi Kamwendo <kondie208@gmail.com>
2025-07-31 11:32:34 +0200

    Initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codelabmw\Tests;
+
+use Codelabmw\LaravelPackageSkeleton\PackageServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     */
+    public function getEnvironmentSetUp($app): void
+    {
+        //
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            PackageServiceProvider::class,
+        ];
+    }
+}
